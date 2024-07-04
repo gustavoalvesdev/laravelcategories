eac47b1711bc59cde18e2c12df0b65698428fdcb
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2024-07-04 16:30:12 -0300

    Factories com relacionamentos

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use App\Models\Task;
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+       $this->call([
+            UserSeeder::class,
+            //TaskSeeder::class
+       ]);
+
+       Category::factory(50)->create();
+       Task::factory(30)->create();
+    }
+}
